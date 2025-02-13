a855020ee767df3d4fa67ac92593e54d3c8c6bea
Ali <dkao228@mail.ru>
2025-02-13 10:54:08 +0500

    поправил

--- generators/1.py
+++ generators/1.py
@@ -2,7 +2,6 @@ def generate_squares(N):
     for i in range(1, N+1):
         yield i ** 2
 
-# Example usage:
 N = int(input("Enter a number N: "))
 for square in generate_squares(N):
     print(square)
